zachpoblete/MouseToys

commit 6ada0919a6ecef3becc083124edb2bce354cc84e

ref: remove named function hotkeys
thisHotkey wasn't being used and that was confusing.

lib/classes.ahk
@@ -27,7 +27,7 @@ class C_KeyWait {
 
 class C_Hotkey {
     static ctrlTab(hk, shouldPressShift) {
-        sendFirstAndLast(thisHotkey) {
+        sendFirstAndLast() {
             Send('{Ctrl Down}' tab)
             hkPrefixKey := HotkeyGetPrefixKey(thisHotkey)
             KeyWait(hkPrefixKey)
@@ -36,11 +36,11 @@ class C_Hotkey {
 
         tab := (shouldPressShift)? '{Shift Down}{Tab}{Shift Up}' : '{Tab}'
 
-        HotIf((thisHotkey) => GetKeyState('Ctrl'))
-        Hotkey(hk, (thisHotkey) => Send(tab))
+        HotIf(() => GetKeyState('Ctrl'))
+        Hotkey(hk, () => Send(tab))
 
         HotIf()
-        Hotkey(hk, (thisHotkey) => sendFirstAndLast(thisHotkey))
+        Hotkey(hk, () => sendFirstAndLast())
     }
 
     class Browser {
@@ -50,9 +50,9 @@ class C_Hotkey {
 
         static hotIfCondition(hotIfExFn) {
             if HasProp(hotIfExFn, 'call') {
-                HotIf((thisHotkey) => this.active() and hotIfExFn())
+                HotIf(() => this.active() and hotIfExFn())
             } else {
-                HotIf((thisHotkey) => this.active())
+                HotIf(() => this.active())
             }
         }
 
@@ -78,11 +78,11 @@ class C_Hotkey {
             }
 
             this.hotIfCondition(hotIfExFn)
-            Hotkey(hk, (thisHotkey) => searchUrlInTab(true))
-            Hotkey('+' hk, (thisHotkey) => searchUrlInTab(false))
+            Hotkey(hk, () => searchUrlInTab(true))
+            Hotkey('+' hk, () => searchUrlInTab(false))
 
-            HotIf((thisHotkey) => hotIfExFn())
-            hotkey(hk, (thisHotkey) => runUrl())
+            HotIf(() => hotIfExFn())
+            hotkey(hk, () => runUrl())
             HotIf()
         }
     }

keyboard.ahk
@@ -25,8 +25,8 @@ PrintScreen:: Send('{LWin Down}{Alt Down}{PrintScreen}{Alt Up}{LWin Up}')
 ;== Restart
 ;== ============================================================================
 
-#F5::
-ProcessRestart(thisHotkey) {
+#F5:: ProcessRestart()
+ProcessRestart() {
     WinExist('A')
 
     winPid := WinGetPID()
@@ -304,9 +304,9 @@ ActivatePowerToysRun() {
 
 #HotIf WinActive('ahk_pid ' WinGetPid.tryCall(K_CLASSES['ZOOM']['TOOLBAR']))
         ; Check if a meeting window is active.
-!=::
-Zoom_ThumbsUpReact(thisHotkey) {
-    Zoom_OpenReactions(thisHotkey)
+!=:: Zoom_ThumbsUpReact()
+Zoom_ThumbsUpReact() {
+    Zoom_OpenReactions()
     Sleep(50)
     
     CoordMode('Mouse', 'Client')
@@ -321,8 +321,8 @@ Zoom_ThumbsUpReact(thisHotkey) {
             ; Prevent the activation of the most recent window when the reaction disappears.
 }
 
-!e::
-Zoom_OpenReactions(thisHotkey) {
+!e:: Zoom_OpenReactions()
+Zoom_OpenReactions() {
     if not WinExist(K_CLASSES['ZOOM']['MEETING']) {
         return
     }
@@ -398,16 +398,16 @@ Zoom_OpenReactions(thisHotkey) {
 ;== ============================================================================
 
 #HotIf GetKeyState('NumLock', 'T')
-d::
-WinOpenDir(thisHotkey) {
+d:: WinOpenDir()
+WinOpenDir() {
     winPid := WinGetPID('A')
     winPath := ProcessGetPath(winPid)
     winDir := RegExReplace(winPath, '\\[^\\]+$')
     Run(winDir)
 }
 
-+d::
-RunSelectedAsDir(thisHotkey) {
++d:: RunSelectedAsDir()
+RunSelectedAsDir() {
     dir := GetSelectedElseExit()
 
     while RegExMatch(dir, '%(.+?)%', &match) {
@@ -422,8 +422,8 @@ RunSelectedAsDir(thisHotkey) {
 ;== App
 ;== ============================================================================
 
-#i::
-OpenSettings(thisHotkey) {
+#i:: OpenSettings()
+OpenSettings() {
     switch ChordInput() {
     case 'i': Send('{LWin Down}i{LWin Up}')
     case 'v': Run('App volume and device preferences', 'C:\Windows')
@@ -445,8 +445,8 @@ GroupAdd('ExplorerWins', 'ahk_class CabinetWClass')
 GroupAdd('PhotoWins', ' ' K_CHARS['LEFT_TO_RIGHT_MARK'] '- Photos$ ahk_exe ApplicationFrameHost.exe')
 GroupAdd('ZoomWins', 'ahk_class ^Z ahk_exe Zoom.exe')
 
-^!Tab::
-IntraSwitchActiveGroup(thisHotkey) {
+^!Tab:: IntraSwitchActiveGroup()
+IntraSwitchActiveGroup() {
     check(groupName) {
         if not WinActive('ahk_group ' groupName) {
             return
@@ -479,8 +479,8 @@ IntraSwitchActiveGroup(thisHotkey) {
 ;== Media
 ;== ============================================================================
 
-Pause::
-OneButtonRemote(thisHotkey) {
+Pause:: OneButtonRemote()
+OneButtonRemote() {
     static pressCount := 0
     
     pressCount++
@@ -563,8 +563,8 @@ Volume_Mute:: vk13
 
 #HotIf not WinActive.bind('ahk_exe .EXE$').setWinModeAndCall('RegEx')
     ; Check if an Office app isn't active.
-Alt::
-MaskAlt(thisHotkey) {
+Alt:: MaskAlt()
+MaskAlt() {
     Send('{Alt Down}')
     Send(K_KEYS['MENU_MASK'])
     KeyWait('Alt')
@@ -577,7 +577,7 @@ MaskAlt(thisHotkey) {
 ;== ============================================================================
 
 if GetKeyState('NumLock', 'T') {
-    NumLockIndicatorFollowMouse('')
+    NumLockIndicatorFollowMouse()
 }
 
 #InputLevel 1
@@ -589,8 +589,8 @@ if GetKeyState('NumLock', 'T') {
 /**
  * Display ToolTip while NumLock is on.
  */
-~*NumLock::
-NumLockIndicatorFollowMouse(thisHotkey) {
+~*NumLock:: NumLockIndicatorFollowMouse()
+NumLockIndicatorFollowMouse() {
     toolTipNumLock() => ToolTip('NumLock On')
 
     Sleep(10)
@@ -609,7 +609,7 @@ NumLockIndicatorFollowMouse(thisHotkey) {
 
 MapF13UntilF24()
 MapF13UntilF24() {
-    remap := (num, thisHotkey) => Send('{Blind}{Alt Up}{Ctrl Up}{F' (num + 12) '}')
+    remap := (num) => Send('{Blind}{Alt Up}{Ctrl Up}{F' (num + 12) '}')
 
     Loop 12 {
         Hotkey('#^!+F' A_Index, remap.bind(A_Index))

mouse.ahk
@@ -105,14 +105,14 @@ RButton:: Click('R')
 ;== Wheel
 ;== ============================================================================
 
-MButton & WheelUp::
-MouseWinMaximize(thisHotkey) {
+MButton & WheelUp:: MouseWinMaximize()
+MouseWinMaximize() {
     MouseWinActivate()
     WinMaximize('A')
 }
 
-MButton & WheelDown::
-MouseWinMinimizeOrRestore(thisHotkey) {
+MButton & WheelDown:: MouseWinMinimizeOrRestore()
+MouseWinMinimizeOrRestore() {
     MouseWinActivate()
     if WinActive(K_CLASSES['ZOOM']['WAIT_HOST']) or WinActive(K_CLASSES['ZOOM']['VID_PREVIEW']) {
         WinMinimize()
@@ -131,8 +131,8 @@ MouseWinMinimizeOrRestore(thisHotkey) {
 ;== RButton
 ;== ============================================================================
 
-MButton & RButton::
-MouseWinMove(thisHotkey) {
+MButton & RButton:: MouseWinMove()
+MouseWinMove() {
     MouseWinActivate()
 
     if WinActive('ahk_class WorkerW ahk_exe Explorer.EXE') {
@@ -157,10 +157,10 @@ MouseWinMove(thisHotkey) {
         }
         switch A_ThisHotkey {
         case 'WheelUp':
-            MouseWinMaximize(thisHotkey)
+            MouseWinMaximize()
             break
         case 'WheelDown':
-            MouseWinMinimizeOrRestore(thisHotkey)
+            MouseWinMinimizeOrRestore()
             break
         }
         MouseGetPos(&mouseX, &mouseY)
@@ -257,8 +257,8 @@ X1LR(states*) {
 ;== ============================================================================
 
 XButton1 & MButton:: return
-XButton1 & MButton Up::
-MouseWinReload(thisHotkey) {
+XButton1 & MButton Up:: MouseWinReload()
+MouseWinReload() {
     if A_PriorKey = 'Escape' {
         return
     }
@@ -328,8 +328,8 @@ XButton2 & RButton Up:: {
 ;== ============================================================================
 
 XButton2 & MButton:: return
-XButton2 & MButton Up::
-MouseWinClose(thisHotkey) {
+XButton2 & MButton Up:: MouseWinClose()
+MouseWinClose() {
     if A_PriorKey = 'Escape' {
         return
     }

script-hotkeys.ahk
@@ -37,8 +37,8 @@
     }
 }
 
-#^h::
-OpenDocs(thisHotkey) {
+#^h:: OpenDocs()
+OpenDocs() {
     if not GetKeyState('CapsLock', 'T') {
         Run('https://lexikos.github.io/v2/docs/AutoHotkey.htm')
         return
@@ -49,8 +49,8 @@ OpenDocs(thisHotkey) {
     Run(query)
 }
 
-#^w::
-WinSpy(thisHotkey) {
+#^w:: WinSpy()
+WinSpy() {
     static WIN_SPY := 'Window Spy ahk_class AutoHotkeyGUI'
 
     try {

commit e44d64a2dce7d1f74e7130ed0900f51c82db3dab

fix(C_Hotkey): reinstate thisHotkey
Broken by https://github.com/zachpoblete/AutoHotkey/commit/6ada0919a6ecef3becc083124edb2bce354cc84e#diff-fe3c4f2f50bfb13de2811f6bdbdfb477297ed11d8a39333ce7f850afd7ee1eda.

lib/classes.ahk
@@ -27,7 +27,7 @@ class C_KeyWait {
 
 class C_Hotkey {
     static ctrlTab(hk, shouldPressShift) {
-        sendFirstAndLast() {
+        sendFirstAndLast(thisHotkey) {
             Send('{Ctrl Down}' tab)
             hkPrefixKey := HotkeyGetPrefixKey(thisHotkey)
             KeyWait(hkPrefixKey)
@@ -36,11 +36,11 @@ class C_Hotkey {
 
         tab := (shouldPressShift)? '{Shift Down}{Tab}{Shift Up}' : '{Tab}'
 
-        HotIf(() => GetKeyState('Ctrl'))
-        Hotkey(hk, () => Send(tab))
+        HotIf((thisHotkey) => GetKeyState('Ctrl'))
+        Hotkey(hk, (thisHotkey) => Send(tab))
 
         HotIf()
-        Hotkey(hk, () => sendFirstAndLast())
+        Hotkey(hk, (thisHotkey) => sendFirstAndLast(thisHotkey))
     }
 
     class Browser {
@@ -50,9 +50,9 @@ class C_Hotkey {
 
         static hotIfCondition(hotIfExFn) {
             if HasProp(hotIfExFn, 'call') {
-                HotIf(() => this.active() and hotIfExFn())
+                HotIf((thisHotkey) => this.active() and hotIfExFn())
             } else {
-                HotIf(() => this.active())
+                HotIf((thisHotkey) => this.active())
             }
         }
 
@@ -78,11 +78,11 @@ class C_Hotkey {
             }
 
             this.hotIfCondition(hotIfExFn)
-            Hotkey(hk, () => searchUrlInTab(true))
-            Hotkey('+' hk, () => searchUrlInTab(false))
+            Hotkey(hk, (thisHotkey) => searchUrlInTab(true))
+            Hotkey('+' hk, (thisHotkey) => searchUrlInTab(false))
 
-            HotIf(() => hotIfExFn())
-            Hotkey(hk, () => runUrl())
+            HotIf((thisHotkey) => hotIfExFn())
+            Hotkey(hk, (thisHotkey) => runUrl())
             HotIf()
         }
     }